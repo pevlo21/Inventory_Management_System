ea680046f8ef6ffe51b9c6d74982acf022b18126
pevlo21 <paulo@aptlylabs.com>
2026-09-16 17:34:15 +0530

    added new atributes to Userbase, reworked JWT token flow in signup

--- api/auth/signup.py
+++ api/auth/signup.py
@@ -19,23 +19,38 @@ def signup_user(
     user: SignupModel,
     db: session
 ):
-    # Check if the username or email already exists
+    # Check if email or phone number already exists
     existing_user = db.exec(
         select(UserBase).where(
-            (UserBase.email == user.email) | (UserBase.phone_number == user.phone_number)
+            (UserBase.email == user.email) |
+            (UserBase.phone_number == user.phone_number)
         )
     ).first()
-
-    print(f"Existing user: {existing_user}")  # Debugging statement``
 
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="Email already exists"
+            detail="Email or phone number already exists"
         )
-    # Hash the password before storing it
-    user.hashed_password = bcrypt_context.hash(user.hashed_password)
-    #create access token for the new user
-    token = create_access_token(user.username, user.id, timedelta(minutes=30))
 
-    return {"access_token": token, "token_type": "bearer", "user": create_user(user, db)}
+    # Create UserBase object
+    new_user = UserBase(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        username=user.username,
+        email=user.email,
+        phone_number=user.phone_number,
+        hashed_password=bcrypt_context.hash(user.password)
+    )
+
+    # Save user to database
+    save_user = create_user(new_user, db)
+
+    # Create access token after database generates the ID
+    _ = create_access_token(
+        save_user.username,
+        save_user.id,
+        timedelta(minutes=30)
+    )
+
+    return save_user
--- schema/signup.py
+++ schema/signup.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, EmailStr
 class SignupModel(BaseModel):
    first_name: str
    last_name: str
+   username: str
    email: EmailStr
    phone_number: str
-   hashed_password: str
+   password: str
